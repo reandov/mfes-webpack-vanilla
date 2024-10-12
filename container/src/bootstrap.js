7578b0d43885db8d0b88c5a8b8101fdfe48a8e78
Evandro Rodrigues <evandro.rodriguespj@gmail.com>
2024-10-12 15:32:54 -0300

    adding project files

---
+++ container/src/bootstrap.js
@@ -0,0 +1,5 @@
+import { mount as productsMount } from 'products/ProductsIndex'
+import { mount as cartMount } from 'cart/CartShow'
+
+productsMount(document.querySelector('#container-products'))
+cartMount(document.querySelector('#container-cart'))
